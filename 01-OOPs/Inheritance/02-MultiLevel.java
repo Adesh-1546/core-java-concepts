cb072226c765df9b5a3e29d1d4203efa321285c2
Adesh Pol <adeshpol1234@gmail.com>
2026-07-07 20:22:38 +0530

    Update 02-MultiLevel.java

--- 01-OOPs/Inheritance/02-MultiLevel.java
+++ 01-OOPs/Inheritance/02-MultiLevel.java
@@ -1,1 +1,40 @@
+class Animal {
+    // Grandparent class
+    public void eat() {
+        System.out.println("Animal is eating");
+    }
+}
 
+class Dog extends Animal {
+    // Parent class
+    public void bark() {
+        System.out.println("Dog is barking");
+    }
+}
+
+class Puppy extends Dog {
+    // Child class
+    public void weep() {
+        System.out.println("Puppy is weeping");
+    }
+}
+
+public class MultilevelInheritanceDemo {
+    public static void main(String[] args) {
+        
+        // Creating object of lowest child class
+        Puppy p = new Puppy();
+        
+        // Accessing all levels of methods
+        p.eat();   // from Animal
+        p.bark();  // from Dog
+        p.weep();  // from Puppy
+    }
+}
+
+/*
+OUTPUT
+Animal is eating
+Dog is barking
+Puppy is weeping
+*/
