acf429d9f56e97d02e87d37759dc143df569645f
Adesh Pol <adeshpol1234@gmail.com>
2026-07-07 20:20:58 +0530

    Update 01-SingleLevel.java

--- 01-OOPs/Inheritance/01-SingleLevel.java
+++ 01-OOPs/Inheritance/01-SingleLevel.java
@@ -1,1 +1,27 @@
+class Animal {
+    // Parent class method
+    public void eat() {
+        System.out.println("Animal is eating");
+    }
+}
 
+// Child class inheriting from Animal
+class Dog extends Animal {
+    public void bark() {
+        System.out.println("Dog is barking");
+    }
+}
+
+public class SingleLevelInheritanceDemo {
+    public static void main(String[] args) {
+        
+        // Creating object of child class
+        Dog d = new Dog();
+        
+        // Calling parent class method
+        d.eat();
+        
+        // Calling child class method
+        d.bark();
+    }
+}
